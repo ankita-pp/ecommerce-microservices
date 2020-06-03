d4456115f765a8972385caba37b0ff5c6d8a9f6c
Pankaj Pande <Pankajvpande@gmail.com>
2020-06-04 03:54:55 +0900

    Rest Template test

---
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -0,0 +1,28 @@
+package com.example.demo.controller;
+
+import com.example.demo.service.TestService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/test")
+public class TestController {
+	private final TestService testService;
+	@GetMapping("/hello")
+	public String getHello() {
+		return "Hello";
+	}
+	@GetMapping("/world")
+	public String getWorld() {
+		return "World";
+	}
+
+	@GetMapping("/rest-template-test")
+	public String getHelloWorld(){
+		return testService.getData();
+	}
+
+}
---
+++ src/main/java/com/example/demo/service/TestService.java
@@ -0,0 +1,15 @@
+package com.example.demo.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+@RequiredArgsConstructor
+public class TestService {
+	private final RestTemplate restTemplate;
+
+	public String getData() {
+		return restTemplate.getForObject("http://localhost:8080/test/hello",String.class)+restTemplate.getForObject("http://localhost:8080/test/world", String.class);
+	}
+}
